43c34096cca9eb8ea31f2ee2934608967b269b7e
Om Rajpal <79391612+omrajpal@users.noreply.github.com>
2024-08-11 14:05:55 -0500

    Time: 14 ms (69.05%) | Memory: 11.5 MB (98.81%) - LeetSync

---
+++ 2433-best-poker-hand/best-poker-hand.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def bestHand(self, ranks, suits):
+        """
+        :type ranks: List[int]
+        :type suits: List[str]
+        :rtype: str
+        """
+        hashmap = {}
+        flush = True
+        pair = False
+        three = False
+        past = suits[-1]
+        for rank, suit in zip(ranks, suits):
+            if suit != past:
+                flush = False
+            hashmap[rank] = 1 + hashmap.get(rank, 0)
+            if hashmap[rank] == 2:
+                pair = True
+            if hashmap[rank] == 3:
+                three = True
+        
+        return "Flush" if flush else "Three of a Kind" if three else "Pair" if pair else "High Card"
+            
+        

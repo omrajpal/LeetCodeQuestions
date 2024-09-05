db7836c7a04fc55cf2082c87431bcf534f6744f5
Om Rajpal <79391612+omrajpal@users.noreply.github.com>
2024-09-05 13:44:08 -0400

    Time: 1039 ms (39.95%) | Memory: 25.2 MB (79.62%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -8,8 +8,8 @@ class Solution(object):
         answer = [0 for _ in range(len(temperatures))]
         for i in range(len(temperatures)):
             temp = temperatures[i]
-            while stack and temp > stack[-1][1]:
-                index, prev = stack.pop()
+            while stack and temp > temperatures[stack[-1]]:
+                index = stack.pop()
                 answer[index] = i - index
-            stack.append([i, temp])
+            stack.append(i)
         return answer

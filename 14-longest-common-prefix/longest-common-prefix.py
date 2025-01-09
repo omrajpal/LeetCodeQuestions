32e041e15a227a835940914df9d853d8ccf0bedb
Om Rajpal <79391612+omrajpal@users.noreply.github.com>
2025-01-09 18:14:55 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (11.17%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -6,21 +6,9 @@ class Solution(object):
         """
         if not strs or not strs[0]:
             return ""
-        l = 0
-        done = False
-        maxL = min([len(s) for s in strs])
-        while True:
-            if l >= maxL: 
-                break
-            c = strs[0][l]
-            for s in strs:
-                if s[l] != c:
-                    done = True
-                    break
-            if done:
-                break
-            l += 1
-        return strs[0][:l]
-
-
-            
+        strs.sort()
+        i = 0
+        while i < min(len(strs[0]), len(strs[-1])) and strs[0][i] == strs[-1][i]:
+            i += 1
+        
+        return strs[0][:i]
